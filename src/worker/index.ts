41efdd65b433f7c9d941917fcfceefe26f9e00ec
AquafinaSun <aquafina.sun@outlook.com>
2025-09-17 16:26:24 +0800

    feat: adjust exported types in worker folder

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,8 +1,6 @@
-export { WebWorkerRenderer as MTextWorkerManager } from './webWorkerRenderer';
-export { MainThreadRenderer } from './mainThreadRenderer';
-export { UnifiedRenderer } from './unifiedRenderer';
-export type { RenderMode } from './unifiedRenderer';
-export type { MTextBaseRenderer as MTextRendererAdapter } from './baseRenderer';
+export * from './webWorkerRenderer';
+export * from './mainThreadRenderer';
+export * from './unifiedRenderer';
+export * from './unifiedRenderer';
+export * from './baseRenderer';
 
-// Re-export types for convenience
-export type { MTextData, TextStyle, ColorSettings } from '../renderer/types';

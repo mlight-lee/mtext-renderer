3496211f57c92a7869cc3d7c4f5bf2d0364ae034
AquafinaSun <aquafina.sun@outlook.com>
2025-09-17 22:40:00 +0800

    feat: use monorepo to organize code

---
+++ packages/example/vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html')
+      },
+      output: {
+        format: 'es'
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['three']
+  },
+  worker: {
+    format: 'es'
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: './node_modules/@mlightcad/mtext-renderer/dist/mtext-renderer-worker.js',
+          dest: 'assets'
+        }
+      ]
+    })
+  ]
+})
